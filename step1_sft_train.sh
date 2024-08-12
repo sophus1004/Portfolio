cd29f43b05b1e5ae9f9ce15e48b618fe7a9a4e61
J. Cho <sophus1004@gmail.com>
2024-08-12 14:12:20 +0000

    2024.08.12 update

--- step1_sft_train.sh
+++ step1_sft_train.sh
@@ -1,5 +1,6 @@
 #SFT 실행
 
+# gemma-2 train set
 python step1_sft_trainer/train.py \
    --use_lora True \
    --use_qlora False \
@@ -17,4 +18,24 @@ python step1_sft_trainer/train.py \
    --warmup_steps 0 \
    --learning_rate 0.00001 \
    --weight_decay 0.0 \
-   --mixed_precision bf16 
+   --mixed_precision bf16
+
+"""
+# Llama-3.1 train set
+python step1_sft_trainer/train.py \
+   --use_lora True \
+   --use_qlora False \
+   --gradient_checkpointing False \
+   --data_name_or_path maywell/ko_wikidata_QA \
+   --model_name_or_path meta-llama/Meta-Llama-3.1-8B-Instruct \
+   --output_dir ./storage/trained_sft_models \
+   --save_strategy epoch \
+   --num_train_epochs 1 \
+   --logging_steps 1 \
+   --per_device_train_batch_size 1 \
+   --gradient_accumulation_steps 1 \
+   --warmup_steps 0 \
+   --learning_rate 0.00001 \
+   --weight_decay 0.0 \
+   --mixed_precision bf16
+"""
--- step2_dpo_train.sh
+++ step2_dpo_train.sh
@@ -1,5 +1,6 @@
 #DPO 실행
 
+# gemma-2 train set
 python step2_dpo_trainer/train.py \
    --use_qlora False \
    --gradient_checkpointing False \
@@ -17,3 +18,22 @@ python step2_dpo_trainer/train.py \
    --learning_rate 0.00001 \
    --weight_decay 0.0 \
    --mixed_precision bf16 \
+
+"""
+# Llama-3.1 train set
+python step2_dpo_trainer/train.py \
+   --use_qlora False \
+   --gradient_checkpointing False \
+   --data_name_or_path xinlai/Math-Step-DPO-10K \
+   --model_name_or_path meta-llama/Meta-Llama-3.1-8B-Instruct \
+   --output_dir ./storage/trained_dpo_models \
+   --save_strategy epoch \
+   --num_train_epochs 1 \
+   --logging_steps 1 \
+   --per_device_train_batch_size 1 \
+   --gradient_accumulation_steps 1 \
+   --warmup_steps 0 \
+   --learning_rate 0.00001 \
+   --weight_decay 0.0 \
+   --mixed_precision bf16 \
+"""
