d9ef950bc504eeb8da699d96eb1bf66e3b3b8d36
J. Cho <sophus1004@gmail.com>
2024-10-28 05:25:52 +0000

    2024.10.28 update

--- llm_dpo_train.sh
+++ llm_dpo_train.sh
@@ -15,25 +15,24 @@ python llm_dpo_trainer/train.py \
    --per_device_train_batch_size 1 \
    --gradient_accumulation_steps 1 \
    --warmup_steps 0 \
-   --learning_rate 0.00001 \
+   --learning_rate 1e-5 \
    --weight_decay 0.0 \
    --mixed_precision bf16 \
 
-"""
+
 # Llama-3.1 train set
-python step2_dpo_trainer/train.py \
-   --use_qlora False \
-   --gradient_checkpointing False \
-   --data_name_or_path xinlai/Math-Step-DPO-10K \
-   --model_name_or_path meta-llama/Meta-Llama-3.1-8B-Instruct \
-   --output_dir ./storage/trained_dpo_models \
-   --save_strategy epoch \
-   --num_train_epochs 1 \
-   --logging_steps 1 \
-   --per_device_train_batch_size 1 \
-   --gradient_accumulation_steps 1 \
-   --warmup_steps 0 \
-   --learning_rate 0.00001 \
-   --weight_decay 0.0 \
-   --mixed_precision bf16 \
-"""
+# python llm_dpo_trainer/train.py \
+#    --use_qlora False \
+#    --gradient_checkpointing False \
+#    --data_name_or_path xinlai/Math-Step-DPO-10K \
+#    --model_name_or_path meta-llama/Meta-Llama-3.1-8B-Instruct \
+#    --output_dir ./storage/trained_dpo_models \
+#    --save_strategy epoch \
+#    --num_train_epochs 1 \
+#    --logging_steps 1 \
+#    --per_device_train_batch_size 1 \
+#    --gradient_accumulation_steps 1 \
+#    --warmup_steps 0 \
+#    --learning_rate 1e-5 \
+#    --weight_decay 0.0 \
+#    --mixed_precision bf16 \
--- llm_sft_train.sh
+++ llm_sft_train.sh
@@ -1,4 +1,4 @@
-#SFT 실행
+# SFT 실행
 
 # gemma-2 train set
 python llm_sft_trainer/train.py \
@@ -16,26 +16,25 @@ python llm_sft_trainer/train.py \
    --per_device_train_batch_size 1 \
    --gradient_accumulation_steps 1 \
    --warmup_steps 0 \
-   --learning_rate 0.00001 \
+   --learning_rate 1e-5 \
    --weight_decay 0.0 \
    --mixed_precision bf16
 
-"""
+
 # Llama-3.1 train set
-python step1_sft_trainer/train.py \
-   --use_lora True \
-   --use_qlora False \
-   --gradient_checkpointing False \
-   --data_name_or_path maywell/ko_wikidata_QA \
-   --model_name_or_path meta-llama/Meta-Llama-3.1-8B-Instruct \
-   --output_dir ./storage/trained_sft_models \
-   --save_strategy epoch \
-   --num_train_epochs 1 \
-   --logging_steps 1 \
-   --per_device_train_batch_size 1 \
-   --gradient_accumulation_steps 1 \
-   --warmup_steps 0 \
-   --learning_rate 0.00001 \
-   --weight_decay 0.0 \
-   --mixed_precision bf16
-"""
+# python llm_sft_trainer/train.py \
+#    --use_lora True \
+#    --use_qlora False \
+#    --gradient_checkpointing False \
+#    --data_name_or_path maywell/ko_wikidata_QA \
+#    --model_name_or_path meta-llama/Meta-Llama-3.1-8B-Instruct \
+#    --output_dir ./storage/trained_sft_models \
+#    --save_strategy epoch \
+#    --num_train_epochs 1 \
+#    --logging_steps 1 \
+#    --per_device_train_batch_size 1 \
+#    --gradient_accumulation_steps 1 \
+#    --warmup_steps 0 \
+#    --learning_rate 1e-5 \
+#    --weight_decay 0.0 \
+#    --mixed_precision bf16
